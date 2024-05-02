bbacc557940c4ea1cc3308486bf2ad8c89d00c6e
QuanN <jeanquan98@gmail.com>
2024-05-02 09:02:07 -0500

    set up App components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets'
+;
 const Navbar = () => {
   return (
     <div>Navbar</div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
