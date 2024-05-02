a8c0aa9f50e24f03a6cb1e60e5f4967e17a9bdb7
QuanN <jeanquan98@gmail.com>
2024-05-02 09:45:11 -0500

    building NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,59 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets'
 ;
 const Navbar = () => {
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  const navList = navLinks.map((link) => (
+    <li 
+      key={link.id}
+      className={`${
+        active === link.title ? 'text-white' : 'text-secondary'
+      } hover:text-white text-[18px] fnt-medium cursor-pointer`}
+      onClick={() => setActive(link.title)}
+    >
+      <a href={`#${link.id}`}>{link.title}</a>
+    </li>
+  ));
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'> 
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0,0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-15 h-9 object-contain'/>
+          <p className='text-white text-[18[x] font-bold cursor-pointer'>Quan Nguyen<span className='sm:block hidden'>| Software Development Engineer</span></p>
+        </Link>
+
+        {/* Desktop */}
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navList}
+        </ul>
+
+        {/* Mobile */}
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navList}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
