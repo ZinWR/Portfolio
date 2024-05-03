e7fd471802f56a7a36c31602bece22890ff5e9a3
QuanN <jeanquan98@gmail.com>
2024-05-03 08:05:23 -0500

    set up intro section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,13 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#9153ff]'>Quan Nguyen</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className='sm:block hidden'/>interfaces, tools and web applications
+          </p>
         </div>
+
       </div>
+        <ComputersCanvas />
     </section>
   );
 };
