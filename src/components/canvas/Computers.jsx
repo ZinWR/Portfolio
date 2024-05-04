7340e58d9fc2831da3bb1a85e7d5e9ceb871271a
QuanN <jeanquan98@gmail.com>
2024-05-04 09:20:45 -0500

    started configuring 3D model for mobile devices

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -28,6 +28,23 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  },[]);
+
   return (
     <Canvas
       frameloop='demand'
