5a1a086ede188a53f8339b1be8a16dc7ba17041e
QuanN <jeanquan98@gmail.com>
2024-05-04 09:24:51 -0500

    Built media query for mobile devices

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
@@ -19,8 +19,8 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -31,15 +31,20 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500');
+    // Add event listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    
+    // Set initial value
     setIsMobile(mediaQuery.matches);
 
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    // Add cb function as listener for changes to media query
     mediaQuery.addEventListener('change', handleMediaQueryChange);
 
+    // Remove when unmounted
     return () => {
       mediaQuery.removeEventListener('change', handleMediaQueryChange);
     }
@@ -59,7 +64,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-          <Computers />
+          <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
