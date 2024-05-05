373606f9f3a7249431e25559053a60c523fe2c50
QuanN <jeanquan98@gmail.com>
2024-05-05 10:56:49 -0500

    Adjust about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn('', '', 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and JavaScript with expertise in frameworks like NextJS, React, Node.js, and Three.js. <br/><br/> I'm a problem-solver and quick learner who loves collaborating closely with clients to create efficient, scalable, and user-friendly solutions and solve real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in TypeScript and JavaScript with expertise in frameworks like NextJS, React, Node, and ThreeJS. <br/><br/> I'm a problem-solver and quick learner who loves collaborating closely with clients to create efficient, scalable, and user-friendly solutions and solve real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
