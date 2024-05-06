5f842476aa97cae3161ca5172a3de625030aa8e8
QuanN <jeanquan98@gmail.com>
2024-05-06 11:59:38 -0500

    added Ball component layout

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,12 @@ import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>Tech
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
   );
 };
