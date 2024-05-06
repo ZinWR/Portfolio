66627b8cf57bb84ff9c4dabecf927827c7428b7b
QuanN <jeanquan98@gmail.com>
2024-05-06 11:50:43 -0500

    initialized Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../higher-order-components';
+import { technologies } from '../constants';
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className='flex flex-row flex-wrap justify-center gap-10'>Tech
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech,'');
