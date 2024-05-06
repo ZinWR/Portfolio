a09991f88a92662600afd17b85d267341d2f8563
QuanN <jeanquan98@gmail.com>
2024-05-06 12:49:14 -0500

    built project card animations and layout

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,10 +7,43 @@ import { SectionWrapper } from '../higher-order-components';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const Works = () => {
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
-    <div>Works</div>
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      test
+    </motion.div>
   );
 };
 
-export default Works;
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Each project is briefly described with links to code repositories and live demos in it. <br/><br /> It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works,'');
