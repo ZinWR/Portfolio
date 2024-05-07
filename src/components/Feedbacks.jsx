7b679adb5ffef782e4af5dbdfd6492c2246a319c
QuanN <jeanquan98@gmail.com>
2024-05-07 14:29:14 -0500

    initialized Feedbacks component

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../higher-order-components';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div>
+      
+    </div>
+  );
+};
 
-export default Feedbacks
+export default Feedbacks;
