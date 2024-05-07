345672f269b04f1b8dc00e24b7b9cd415bc34235
QuanN <jeanquan98@gmail.com>
2024-05-07 14:31:55 -0500

    added headers

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,8 +7,13 @@ import { testimonials } from '../constants';
 
 const Feedbacks = () => {
   return (
-    <div>
-      
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
     </div>
   );
 };
