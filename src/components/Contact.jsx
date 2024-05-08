d9527ca9fe80842585f99424e23640d85e43d267
QuanN <jeanquan98@gmail.com>
2024-05-08 12:29:47 -0500

    built headers for Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,28 @@ import { SectionWrapper } from '../higher-order-components';
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handlChange = (e) => {}
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+        variants={slideIn('left','tween', 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contacts.</h3>
+      </motion.div>
+    </div>
   );
 };
 
-export default Contact;
+export default SectionWrapper(Contact,"contact");
